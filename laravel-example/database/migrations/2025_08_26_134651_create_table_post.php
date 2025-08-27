004db7c71fbf2ad440d32641bc717b20a8e39f26
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-08-27 13:54:48 +0000

    feat: :sparkles: Factorias y Seeders, insertar datos de prueba (fake)

--- laravel-example/database/migrations/2025_08_26_134651_create_table_post.php
+++ laravel-example/database/migrations/2025_08_26_134651_create_table_post.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('contet');
+            $table->text('content');
             $table->boolean('status');
             $table->timestamps();
         });
