d894d714ecb37bb64915a9d4724dd62a3f240693
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-08-28 14:04:21 +0000

    feat: :sparkles: Empezando relacion Mcuhos a Muchos con Pivote

--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -4,13 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDelete;
 
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDelete;
     protected $table = "posts";
 
-    protected $fillable = ['title', 'content', 'status'];
+    protected $fillable = [
+        'title', 'content', 'status',
+        'published_at', 'cover_image','tags','meta'
+    ];
 
+    protected $casts = [
+        'published_at' => 'datetime',
+        'tags' => 'array',
+        'meta' => 'meta'
+    ];
+
+    public function categories(){
+        //Tabla Pivote post_category
+
+        return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimeStamps();
+    }
 }
