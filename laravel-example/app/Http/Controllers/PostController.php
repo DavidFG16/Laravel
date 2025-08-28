42207bbe13a936cfb33cea7c42cca4d5d2ac619b
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-08-28 13:48:53 +0000

    feat: :sparkles: Comenzando a implementar el CRUD, Post y Get

--- laravel-example/app/Http/Controllers/PostController.php
+++ laravel-example/app/Http/Controllers/PostController.php
@@ -4,14 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use App\Models\Post;
+// TODO: Eliminar
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     use ApiResponse;
-    
-    public function __invoke()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
+        // La mala practica porque tenemos un Model.
+        // return response()->json(DB::table("posts")->get());
+        return $this->ok("Todo ok, como dijo el Pibeee", Post::get());
+    }
 
-        return $this->ok("Desde invoke");
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $newPost = Post::create($request->only(['title','content','status'])); // Simplemente inserta sin validaciones, ni comparacion, ni actualizacion
+        $newPost = Post::updateOrCreate(
+            ['title'  => $request->title], // las columnas parwa la validacion
+            $request->only(['title','content','status'])); // las columnas para reistrar o actualizar
+        return $this->ok("Todo melo basuras", [$newPost]);
+
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $result = Post::find($id);
+        if ($result){
+            return $this->ok("Todo ok, como dijo el Pibeee", $result);
+        } else{
+            return $this->success("Todo mal, como NO dijo el Pibeee", [], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
