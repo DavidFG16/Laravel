63cac606e53c246b1648d613c95a68040f616ac6
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-08-29 14:05:12 +0000

    feat: :sparkles: Terminando relacion de muchos a muchos, y finalizzando migraciones

---
+++ laravel-example/app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function post(){
+        return $this->belongsToMany(Post::class)->using(CategoryPost::class)->withTimeStamps();
+    }
+}
--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDelete;
+use App\Models\Category;
 
 
 class Post extends Model
@@ -25,7 +26,6 @@ class Post extends Model
 
     public function categories(){
         //Tabla Pivote post_category
-
         return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimeStamps();
     }
 }
